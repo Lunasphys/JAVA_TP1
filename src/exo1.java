363ac54c69ef9b330858832b45cc82b1c9563b16
Lunasphys <91267490+Lunasphys@users.noreply.github.com>
2022-09-20 10:58:44 +0200

    exo 1 ok

--- src/exo1.java
+++ src/exo1.java
@@ -1,4 +1,4 @@
-import checkInputIsInt.checkIntegerInput;
+import checkInputIsInt.CheckIntegerInput;
 
 import java.lang.*;
 
@@ -6,9 +6,11 @@ import java.lang.*;
 public class exo1 {
     public static void main (String[] args) {
         System.out.println("Bienvenue sur le premier exerice");
-        System.out.println("Veuillez entrer une valeur positive");
-        int nb = checkIntegerInput.entierInt();
-        System.out.println("aled" );
+        System.out.println("Entrer un entier positif (0 pour quitter l'exercice)");
+        int nb = CheckIntegerInput.entierInt();
+        if (nb == 0) {
+            //RETOURNER AU MENU NE PAS OUBLIER
+        }
         double r = Math.sqrt(nb);
         System.out.println("La racine carre de "+nb+ " est " + r );
     }
