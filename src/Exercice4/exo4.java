059f21a6cdaa495307a9b6020c8f15a719698b89
MaxYnovobry <maxime-obry@hotmail.fr>
2022-09-23 12:30:05 +0200

    Endofexo4

--- src/Exercice4/exo4.java
+++ src/Exercice4/exo4.java
@@ -6,9 +6,12 @@ public class exo4 {
 
     public static void main(String[] args) {
         System.out.println("Entrez le nombre de terme de la suite à calculer n avec n > 0 (0 pour terminer) :");
-        double nb = CheckIntegerInput.entierInt();
-        double res ;
-        res = nb - 1 + 1 / nb;
-        System.out.println("U"+nb+" est : "+ res);
+        float nb = CheckIntegerInput.entierInt();
+        double U = 0 ;
+        for (float n = 1; n <= nb ; n++) {
+            U = U   + 1 / n;
+        }
+
+        System.out.println("U"+nb+" est : "+ (double)Math.round(U *  10000) / 10000);
     }
 }
---
+++ src/Exercice6/exo6.java
@@ -0,0 +1,8 @@
+package src.Exercice6;
+
+public class exo6 {
+    public static void main(String[] args) {
+
+    }
+
+}
