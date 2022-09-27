936cff239464bd6f644b97de253a83e3164190f3
Lunasphys <91267490+Lunasphys@users.noreply.github.com>
2022-09-27 09:57:49 +0200

    Menu et exo 5 fin

--- src/Menu.java
+++ src/Menu.java
@@ -19,34 +19,34 @@ public class Menu {
         System.out.println("======== Exercice 6 : Tableau ===================");
         System.out.println("Quel exercice souhaitez-vous faire ?");
         int value = CheckIntegerInput.entierInt();
-        switch (value) {
-            case 1:
-                src.Exercice1.exo1.main(args);
-                Menu.main(args);
-                break;
-            case 2:
-                src.Exercice2.exo2.main(args);
-                Menu.main(args);
-                break;
-            case 3:
-                src.Exercice3.exo3.main(args);
-                Menu.main(args);
-                break;
-            case 4:
-                src.Exercice4.exo4.main(args);
-                Menu.main(args);
-                break;
-            case 5:
-                src.Exercice5.exo5.main(args);
-                Menu.main(args);
-                break;
-            case 6:
-                src.Exercice6.exo6.main(args);
-                Menu.main(args);
-                break;
-            default:
+        while (true) {
+            switch (value) {
+                case 1:
+                    src.Exercice1.exo1.main(args);
+                    Menu.main(args);
+                    break;
+                case 2:
+                    src.Exercice2.exo2.main(args);
+                    Menu.main(args);
+                    break;
+                case 3:
+                    src.Exercice3.exo3.main(args);
+                    Menu.main(args);
+                    break;
+                case 4:
+                    src.Exercice4.exo4.main(args);
+                    Menu.main(args);
+                    break;
+                case 5:
+                    src.Exercice5.exo5.main(args);
+                    Menu.main(args);
+                    break;
+                case 6:
+                    src.Exercice6.exo6.main(args);
+                    Menu.main(args);
+                    break;
+        }
                 System.out.println("Veuillez entrer un nombre entre 1 et 6");
-                Menu.main(args);
                 break;
         }
 
